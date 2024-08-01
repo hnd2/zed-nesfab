1ca29d42c5a2eb9141b5673f8559c371228fc8f4
hnd2 <helloworld.zip@gmail.com>
2024-08-01 11:52:55 +0900

    update tree-sitter

--- languages/nesfab/outline.scm
+++ languages/nesfab/outline.scm
@@ -6,10 +6,20 @@
     "vars" @context
     (group_identifier) @name) @item
 
+(vars_definition
+    "vars" @context) @item
+
 (data_definition
     "data" @context
     (group_identifier) @name) @item
 
 (function_definition
-    "fn" @context
+    (function_keyword) @context
     (identifier) @name) @item
+
+(asm_function_definition
+    (asm_function_keyword) @context
+    (identifier) @name) @item
+
+(chrrom_definition
+    "chrrom" @context) @item
